2fa15c434745b9b0335eac09fbbd827e826b0ea4
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-02-06 07:16:14 +0500

    Update extra.js

--- commands/extra.js
+++ commands/extra.js
@@ -1,22 +1,13 @@
-/*
- *
- ██╗███████╗██╗   ██╗██╗  ██╗██╗   ██╗    ███╗   ███╗██████╗ 
-██║╚══███╔╝██║   ██║██║ ██╔╝██║   ██║    ████╗ ████║██╔══██╗
-██║  ███╔╝ ██║   ██║█████╔╝ ██║   ██║    ██╔████╔██║██║  ██║
-██║ ███╔╝  ██║   ██║██╔═██╗ ██║   ██║    ██║╚██╔╝██║██║  ██║
-██║███████╗╚██████╔╝██║  ██╗╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
-╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
-                                                            
+/**
  Copyright (C) 2022.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : MALIK-MD
+ * @author : arkhan998 <https://github.com/arkhan998>
+ * @description : malik,A Multi-functional whatsapp bot.
  * @version 0.0.6
- *
- */
+ **/
 
 const {cmd} = require('../lib')
 const PastebinAPI = require("pastebin-js");
